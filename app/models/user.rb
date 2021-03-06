30525d31e535038add97b8e73bacaa24d0f16bc4
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-07 04:23:29 +0900

    新規登録、ログインログアウト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
       
           validates :nickname, presence: true
           validates :email, presence: true, uniqueness: true
-          validates :encrypted_password, presence: true, length: { minimum: 6 }, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} 
+          validates :password, presence: true, length: { minimum: 6 }, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} 
           with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
             validates :last_name
             validates :first_name
