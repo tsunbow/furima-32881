7112a5d3855f10d79f4b4c955cad34a7b9dbea4b
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-10 22:22:36 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,21 @@
 class Item < ApplicationRecord
+  
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :text
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :area_id
+    validates :date_id
+  end
+  validates :price, presence: true, numericality: { only_integer: true,
+  greater_than: 300, less_than: 9999999
+ }
+  
   belongs_to :user
   # has_one :purchases
   has_one_attached :image
-  
+
 end
