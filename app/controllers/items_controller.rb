e1a795959393e64a72374084b90b635c821c06ea
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-12 04:23:17 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :item_name, :text, :category_id, :condition_id, :cost_id, :area_id, :date_id, :price)
   end
 
 end
