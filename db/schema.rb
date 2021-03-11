c2046f07107987a03e4022fab18fce0409dbe493
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-12 03:33:59 +0900

    カラム名修正
    カラム名の影響で、active_hashでエラーが起きていたので

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_185900) do
+ActiveRecord::Schema.define(version: 2021_03_11_183149) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_185900) do
     t.integer "condition_id", null: false
     t.integer "cost_id", null: false
     t.integer "area_id", null: false
-    t.integer "date_id", null: false
+    t.integer "datee_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
