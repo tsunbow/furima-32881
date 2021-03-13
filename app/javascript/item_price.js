48177de57922669e3d632cd6bc61f5859387db22
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-14 05:11:07 +0900

    販売手数料表示実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+    console.log(addTaxDom);
+
+    const salesProfit = document.getElementById("profit");
+    salesProfit.innerHTML = (Math.floor(inputValue - addTaxDom.innerHTML));
+    console.log(salesProfit);
+  });
+});
