027c64ee1e4c8b211c5ffb0674c51c8bfa02085a
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-22 04:09:25 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
   
 
