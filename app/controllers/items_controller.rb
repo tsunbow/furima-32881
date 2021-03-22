6900f37df80a962193c47e1845c8e384930c5565
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-22 16:39:01 +0900

    削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :redirect_root, only: [:edit, :update, :destroy]
 
   def index
