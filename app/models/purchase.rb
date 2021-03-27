8fac63ea3bc822651a73bcf06546437a5fc10c1b
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-28 02:29:51 +0900

    データをテーブルに保存する処理の記述

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -11,10 +11,10 @@ class Purchase
   end
 
   def save
-
-
+    user =  User.create(nickname: nickname, email: email, password: password, first_name: first_name, family_name: family_name, first_name_kana: first_name_kana, family_name_kana: family_name_kana, birth_day: birth_day)
+    Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, house_number: house_number, phone_number: phone_number, building_name: building_name, user_id: user.id)
   end
-  
+
   # belongs_to :user
   # belongs_to :item
   # has_one :address
