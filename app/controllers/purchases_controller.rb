15785f72166528764c8c0d878f00cca8cd95cff7
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-03-28 04:24:05 +0900

    ビューファイルの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,11 @@
 class PurchasesController < ApplicationController
+
   def index
+    @purchase = Purchase.new
   end
 
   def create
   end
-  
+
 
 end
--- db/migrate/20210327133536_create_addresses.rb
+++ db/migrate/20210327133536_create_addresses.rb
@@ -8,6 +8,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string  :phone_number,  null: false
       t.string  :building_name
       t.references :user,       null: false, foreign_key: true
+      t.references :purchase ,  null: false, foreign_key: true
       t.timestamps
     end
   end
